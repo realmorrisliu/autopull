09f55966c5dc4e58e1ca1e12b8ab56f48f421011
Morris Liu <morrislew@outlook.com>
2017-09-22 23:01:05 +0800

    bug fix: timeout bug

--- autopull.py
+++ autopull.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import time
-from github import Github
 
 configPath = os.path.join(os.path.expanduser('~'), '.autopullconfig')
 config = {
@@ -31,31 +30,34 @@ except FileNotFoundError:
 
         CONFIG.write('username:{0}\npassword:{1}\nrepo:{2}\nproj:{3}\n'.format(username, password, repo, proj))
 
-repo = Github(username, password).get_user().get_repo(repo)
 os.chdir(proj)
 
-isUpToDate = True
-
 try:
+    isUpToDate = True
     while True:
         with open('.git/HEAD', 'r') as HEAD:
             branchName = os.path.split(HEAD.readline().strip())[1]
 
-        try:
-            branch = repo.get_branch(branchName)
-        except TimeoutError:
-            repo = Github(username, password).get_user().get_repo(repo)
-
+        command = 'curl -s -i -u {0}:{1} https://api.github.com/repos/{0}/{2}/branches/{3}'.format(username,
+                                                                                                   password,
+                                                                                                   repo,
+                                                                                                   branchName)
+        with os.popen(command) as response:
+            for line in response.readlines():
+                if line.find('sha') > -1:
+                    commit = line.split('"')[3]
+                    break
         if isUpToDate:
             sys.stdout.write("\r" + time.asctime(time.localtime(time.time())))
             sys.stdout.flush()
             with open('.git/logs/refs/heads/{0}'.format(branchName), 'r') as refsHEAD:
                 for line in refsHEAD.readlines():
-                    isUpToDate = line.find(branch.commit.sha) > -1
+                    isUpToDate = line.find(commit) > -1
         else:
             print('\nSyncing...')
             os.system('git pull')
             isUpToDate = True
+            os.system('clear')
 
 except KeyboardInterrupt:
     print('\nTerminated')
